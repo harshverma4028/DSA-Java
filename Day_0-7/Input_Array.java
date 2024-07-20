f6e1159ae3ab8e64f564919772243a88d7eed51f
Harsh Kumar <97897760+harshverma4028@users.noreply.github.com>
2024-07-20 15:51:47 +0530

    Searching

--- Day_0-7/Input_Array.java
+++ Day_0-7/Input_Array.java
@@ -21,5 +21,16 @@ public class Input_Array {
         for(int i=0;i<myArray3.length;i++){
             System.out.println(myArray3[i]);
         }
+
+
+        //Searching in array
+        System.out.println("Enter the element to be searched: ");
+        int search=sc.nextInt();
+        for(int i=0;i<myArray3.length;i++){
+            if(myArray3[i]==search){
+                System.out.println("Element found at index: "+i);
+                break;
+            }
+        }
     }
 }
