9e336634ac382ff6ef159f235cc18e3fa7801200
Harsh Kumar <97897760+harshverma4028@users.noreply.github.com>
2024-07-20 11:41:50 +0530

    second day commit

---
+++ Day_0-7/Input_Array.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Input_Array {
+    public static void main(String args[]){
+        System.out.println("Enter the length of the array: ");
+
+        //Array Declaration
+        Scanner sc=new Scanner(System.in);
+        int size=sc.nextInt();
+        int [] myArray3=new int[size];
+        
+        
+        
+        System.out.println("Enter the elements of the array: ");
+        for(int i=0;i<myArray3.length;i++){
+            myArray3[i]=sc.nextInt();
+
+        }
+       
+        System.out.println("Elements of arrays are :");
+        for(int i=0;i<myArray3.length;i++){
+            System.out.println(myArray3[i]);
+        }
+    }
+}
