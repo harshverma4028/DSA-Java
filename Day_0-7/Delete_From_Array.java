3c1d8fe56fef9ac6d4e97abcc6883fbe601dd81b
Harsh Kumar <97897760+harshverma4028@users.noreply.github.com>
2024-07-21 21:38:36 +0530

    day 3 commit

---
+++ Day_0-7/Delete_From_Array.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Delete_From_Array {
+    public static void main(String[] args) {
+        int arrd[]={1,2,3,4,5,6,7,8,9,10};
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number you want to delete: ");
+        int x=sc.nextInt();
+        
+    }
+}
