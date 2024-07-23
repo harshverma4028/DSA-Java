a68ed7718e4e1265b3bfc5b35fce6789321a46d9
Harsh Kumar <97897760+harshverma4028@users.noreply.github.com>
2024-07-23 11:32:55 +0530

    day 4.2

--- Day_0-7/Sum_And_Average.java
+++ Day_0-7/Sum_And_Average.java
@@ -1,15 +1,21 @@
 import java.util.Arrays;
 
-public class Sum_of_Array {
+public class Sum_And_Average{
     public static void main(String[] args) {
         int arras[]={2,5,7,5,2,5};
         int sum=0;
-
+   
+        //sum of array 
         for(int i=0;i<arras.length;i++){
             sum+=arras[i];
         }
 
         System.out.println("The Array is :"+Arrays.toString(arras));
         System.out.println("The sum of Array is : "+sum);
+
+        //Average of arary 
+        int length=arras.length;
+        double average=sum/length;
+        System.out.println("the average of the Array is :"+average);
     }
 }
