c8bcc8ca81649b96cda2ca540345939c4a81f787
Harsh Kumar <97897760+harshverma4028@users.noreply.github.com>
2024-07-24 23:41:01 +0530

    day6.4

--- Day_0-7/Occurence.java
+++ Day_0-7/Occurence.java
@@ -1,23 +1,27 @@
 import java.util.Arrays;
 
 public class Occurence {
-    static void getOccurence(int[] arr, int n){
+    static void getOccurence(int[] arr){
 
         Arrays.sort(arr);
     for(int i=0;i<arr.length;i++){
         int count=1;
+        // if (i!=arr.length-1) {
+            
+        
         for(int j=i+1;j<arr.length-1;j++){
             if (arr[i]==arr[j]) 
                 count++;
             else break; 
             
         }
+    // }
         System.out.println("The number "+arr[i]+" occurs "+count+" times");
         i+=(count-1);
     }
 }
     public static void main(String[] args) {
-        int arr[]={1,2,3,4,5,5,2,8,8,};
-        getOccurence(arr, 5);
+        int arr[]={2,3,3,3,2,2,2,1,3,2,1};
+        getOccurence(arr);
     }
 }
