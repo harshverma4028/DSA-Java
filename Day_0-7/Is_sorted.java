a162c937257adf9a5a56e3c52290182b78727fa2
Harsh Kumar <97897760+harshverma4028@users.noreply.github.com>
2024-07-24 22:44:31 +0530

    day6.1

---
+++ Day_0-7/Is_sorted.java
@@ -0,0 +1,16 @@
+public class Is_sorted {
+
+    static boolean isSorted(int[] arr){
+        for(int i=1;i<arr.length;i++){
+            if (arr[i]>arr[i-1]) {
+                return true;
+            }else{
+                return false;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,5,6,7,8,9,};
+        System.out.println(isSorted(arr));
+    }
+}
