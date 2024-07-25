f301259bddff99df16d546fdbce479c6944fa9c7
Harsh Kumar <97897760+harshverma4028@users.noreply.github.com>
2024-07-25 22:52:14 +0530

    7.2

---
+++ Day_0-7/Is_Pallindrome.java
@@ -0,0 +1,25 @@
+public class Is_Pallindrome {
+
+    static void isPallindrome(int arr[],int n){
+        //Initilazing the flag to true
+        int flag=0;
+        //Checking if the array is null
+        for(int i=0;i<arr.length/2 && n!=0;i++){
+            if (arr[i]!=arr[arr.length-i-1]) {
+                flag=1;
+                break;
+            }
+        }
+        if (flag==1) {
+            System.out.println("The array is not a pallindrome");
+        }else{
+            System.out.println("The array is a pallindrome");
+        }
+
+    }
+    public static void main(String[] args) {
+        int arr[]={1,2,3,2,1,2};
+        isPallindrome(arr, arr.length);
+    }
+
+}
