6f05b987ede978ae1f2801732d0239dddde9ad25
Harsh Kumar <97897760+harshverma4028@users.noreply.github.com>
2024-08-05 19:27:43 +0530

    14.1

---
+++ Day_7_14/Table.java
@@ -0,0 +1,19 @@
+package Day_7_14;
+import java.util.Scanner;
+
+public class Table {
+    public static void main(String[] args) {
+        System.out.println("Enter a number: ");
+        table(0);
+        
+    }
+
+    public static void table(int num){
+        Scanner sc = new Scanner(System.in);
+        num = sc.nextInt();
+        for (int i=1; i<=10; i++){
+            System.out.println(num + "*"+i+"="+num*i);
+        }
+
+    }
+}
