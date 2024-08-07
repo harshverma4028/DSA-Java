f30a65049180306096c1a3c97e7f7e7b512ff333
Harsh Kumar <97897760+harshverma4028@users.noreply.github.com>
2024-08-07 19:43:15 +0530

    15.1

---
+++ Day_7_14/Break.java
@@ -0,0 +1,19 @@
+package Day_7_14;
+import java.util.Scanner;
+
+public class Break {
+  public static void main(String[] args) {
+     try (Scanner input = new Scanner(System.in)) {    
+
+      while(true){
+        System.out.println(" enter your command " );
+        
+      String command = input.nextLine();
+        if (command.equalsIgnoreCase("exit")) {
+          break;
+        }
+      }
+      System.out.println("Exit successfully...");
+     }
+  }
+}
