b7a8aa739d19b77733c652c0e850d6c6222afe9f
Harsh Kumar <97897760+harshverma4028@users.noreply.github.com>
2024-08-09 20:47:28 +0530

    18.1

---
+++ Day_7_14/For_Each.java
@@ -0,0 +1,11 @@
+package Day_7_14;
+
+public class For_Each {
+    public static void main(String[] args) {
+        String  arr[]={"BMW", "Audi", "Ford", "Toyota", "Honda"};
+        for(String car:arr){
+            System.out.println(car);
+        }
+    }
+}
+
