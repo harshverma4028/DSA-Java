0d2d2079008c8efd67ba9aa3392a9814fe32c7bf
Harsh Kumar <97897760+harshverma4028@users.noreply.github.com>
2024-08-14 20:39:42 +0530

    21.1

---
+++ Day_0-7/Simple_Calculator.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Simple_Calculator {
+    public static void main(String[] args) {
+     try (Scanner input = new Scanner(System.in)) {
+      System.out.println("Enter the number1 : ");
+      int num1 = input.nextInt();
+      System.out.println("Enter the number2 : ");
+      int num2 = input.nextInt();
+      System.out.println("Enter the opcode ");
+      String opcode = input.next();
+      int result;
+      switch (opcode) {
+          case "+":
+              result = num1 + num2;
+              break;
+          case "-":
+              result = num1 - num2;
+              break;
+          case "*":
+              result = num1 * num2;
+              break;
+          case "/":
+              result = num1 / num2;
+              break;
+          case "%":
+              result = num1 % num2;
+              break;
+          default:
+              result = -1;
+              break;
+      }
+      System.out.println(result);      
+
+     }
+    }
+}
