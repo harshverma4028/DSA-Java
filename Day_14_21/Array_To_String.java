0a5b976befb7be2b0abae8f6742092ce16cf2142
Harsh Kumar <97897760+harshverma4028@users.noreply.github.com>
2024-08-17 20:34:44 +0530

    23.1

---
+++ Day_14_21/Array_To_String.java
@@ -0,0 +1,12 @@
+public class Array_To_String {
+    public static void main(String[] args) {
+        String[] arr = new String[]{
+          "my name is", "Harsh kumar", "and" ,"i am study in the", "AVIT college" ,"chennai "
+        };
+        StringBuilder sb = new StringBuilder();
+        for (String string : arr) {
+         sb.append(string).append(" ");
+        }
+        System.out.println(sb);
+      }
+}
