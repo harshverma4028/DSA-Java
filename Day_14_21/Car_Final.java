1fb7692c192b327ef1677a995b9828b3ce6faf3f
Harsh Kumar <97897760+harshverma4028@users.noreply.github.com>
2024-08-18 21:03:39 +0530

    0.0

---
+++ Day_14_21/Car_Final.java
@@ -0,0 +1,11 @@
+public class Car_Final {
+    final int onOfWheels;
+    final String model;
+    final String engineInLitters;
+    
+    public Car_Final(int onOfWheels, String model, String engineInLitters) {
+      this.onOfWheels = onOfWheels;
+      this.model = model;
+      this.engineInLitters = engineInLitters;
+    }
+}
