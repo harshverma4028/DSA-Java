30096b5a2c6c1a1f515cfc9d24663b58ef7d8649
Harsh Kumar <97897760+harshverma4028@users.noreply.github.com>
2024-11-08 21:30:48 +0530

    total pair

--- Array/Pair.java
+++ Array/Pair.java
@@ -3,17 +3,23 @@ package Array;
 
 public class Pair {
     public static void MakePair(int number[]){
+
+        int tp=0;
+
         for(int i=0;i<number.length;i++){
             int curr=number[i];
             for(int j=i+1;j<number.length;j++){
                 System.out.print("("+curr+","+number[j]+")"); 
+                tp++;
             }
             System.out.println();
         }
+        System.out.println("Total Pair: "+tp);
     }
     
     public static void main(String[] args) {
         int number[]={1,2,3,4,5,6,7,8,9,10};
         MakePair(number);
+        
     }
 }
