1d071823088bc2f716ebcf5a28f99a2e65e3e07f
Harsh Kumar <97897760+harshverma4028@users.noreply.github.com>
2024-11-10 23:18:49 +0530

    subarrcount total

--- Array/Subarray.java
+++ Array/Subarray.java
@@ -2,6 +2,7 @@ package Array;
 
 public class Subarray {
     public static void printSubarry(int number[]){
+        int ts=0;
         for(int i=0;i<number.length;i++){
             int start=i;
             for(int j=i+1;j<number.length;j++){
@@ -9,10 +10,13 @@ public class Subarray {
                 for(int k=start;k<end;k++){
                     System.out.print(number[k]+" ");   
                 }
+                ts++;
                 System.out.println();
             }
             System.out.println();
         }
+
+        System.out.println("Total Subarray: "+ts);
     }
     public static void main(String[] args) {
         int number[]={1,2,3,4,5,6,7,8,9,10};
