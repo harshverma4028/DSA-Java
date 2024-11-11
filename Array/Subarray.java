47b3ab42ff35121e87b6d158d9ff7dcc57eab2f6
Harsh Kumar <97897760+harshverma4028@users.noreply.github.com>
2024-11-11 22:12:00 +0530

    a

--- Array/Subarray.java
+++ Array/Subarray.java
@@ -3,17 +3,24 @@ package Array;
 public class Subarray {
     public static void printSubarry(int number[]){
         int ts=0;
+
         for(int i=0;i<number.length;i++){
+
+            
             int start=i;
+
             for(int j=i+1;j<number.length;j++){
                 int end=j;
                 for(int k=start;k<end;k++){
                     System.out.print(number[k]+" ");   
                 }
                 ts++;
+
                 System.out.println();
+                
             }
             System.out.println();
+
         }
 
         System.out.println("Total Subarray: "+ts);
