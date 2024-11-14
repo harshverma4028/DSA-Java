5df02b1d4f394839f3f560da56ceaba6b3ae09ce
Harsh Kumar <97897760+harshverma4028@users.noreply.github.com>
2024-11-14 23:15:47 +0530

    sbarr2

--- Array/SubAr.java
+++ Array/SubAr.java
@@ -1,19 +1,27 @@
 package Array;
 
-public class SubArraySum {
+public class SubAr {
     public static void printSubarrysum(int number[]){
         int currSum=0;
         int maxSum = Integer.MIN_VALUE;
+        int prefix[]=new int[number.length];
+
+        prefix[0]=number[0];
+        // calculate prefix arra
+
+        for(int i=1;i<prefix.length;i++){
+            prefix[i]=prefix[i-1]+number[i];
+        }
 
         for(int i=0;i<number.length;i++){    
             int start=i;
             for(int j=i;j<number.length;j++){
                 int end=j;
-                currSum=0;
-                for(int k=start;k<=end;k++){
-                    currSum+=number[k];  
-                }
-                System.out.println(currSum);
+                currSum= start==0 ? prefix[end]: prefix[j]-prefix[i-1];   
+                // for(int k=start;k<=end;k++){
+                //     currSum+=number[k];  
+                // } 
+                // System.out.println(currSum);
                 if(maxSum<currSum){
                     maxSum=currSum;
                     
